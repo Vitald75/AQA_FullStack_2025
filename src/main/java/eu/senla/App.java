75896c81ab5d178b1c1d3cfd9a5f75f63f9960fe
Vitali_home <Vitald75@gmail.com>
2025-04-14 07:38:26 +0300

    pom and checkstyle

--- src/main/java/eu/senla/App.java
+++ src/main/java/eu/senla/App.java
@@ -1,13 +1,9 @@
 package eu.senla;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+/** Hello world! */
+public class App {
+  public static void main(String[] args) {
+    System.out.println(
+        "Hello World! This is the longest string that i've ever had, and kjhjkh jkhsdfjkh jksdhjkhkjh ksdfhkhusefjhjkhsdfkjh sfkekuh kjhdsfku fkdjyhufsk hksfeuh kjhksfeku");
+  }
 }
